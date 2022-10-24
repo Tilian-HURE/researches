e80dda1168fc2f234d16e793ec6f0236c72271b4
Tilian <onsenfou>
2022-10-24 11:24:50 +0200

    updated integral_calculation typed Louis's code

--- python/integral_calculation/integral_calculation_louis.py
+++ python/integral_calculation/integral_calculation_louis.py
@@ -2,8 +2,8 @@ from math import exp, log, pow
 from numpy import arange
 
 
-# C'est l'exo 5.5 page 3*4 (Pour Titi💓(Genre Thierry, je me rends compte que ce que t'as écrit porte à confusion et vu que t'as signé le fichier à mon nom c'est chelou. La bise))
-def integRec(m, a, b, rectangles):
+# C'est l'exo 5.5 page 3*4 (Pour Titi💓 (Genre Thierry, je me rends compte que ce que t'as écrit porte à confusion et vu que t'as signé le fichier à mon nom c'est chelou. La bise))
+def integRec(m:float, a:float, b:float, rectangles:int)->float:
   area = 0
   step = pow(1,rectangles)
   for i in arange(a, b, step):
@@ -11,4 +11,4 @@ def integRec(m, a, b, rectangles):
   return area
 
 
-print("\033[94mintegRec :\033[92m", integRec(log(2)/200, 0, 300,  1000),  '\033[0m') # Censé être ~0.3535
+print("\033[94mintegRec :\033[92m", 1-integRec(log(2)/200, 0, 300,  1000),  '\033[0m') # Censé être ~0.3535
