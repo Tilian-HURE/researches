07a0034b6dd3544c78252d7ba54a06ceea18bda3
Tilian HURÉ <58342016+Tilian-HURE@users.noreply.github.com>
2022-10-29 22:32:38 +0200

    Update main.py

--- python/permutations/main.py
+++ python/permutations/main.py
@@ -3,6 +3,6 @@ from itertools import permutations
 
 
 ## perms = permutations("LAURENT DEMAY")
-perms = permutations_louis.permutations(list("L"), 0, 100)
+perms = permutations_louis.permutations(list("LAURENT DEMAY"), 0, 100)
 print(*perms)
 print(len(perms))
